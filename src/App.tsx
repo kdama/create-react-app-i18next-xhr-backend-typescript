0fd7a77494b6a93216ce6be7bdfa3728977adc6f
Kai Kodama <kdama@users.noreply.github.com>
2020-11-18 16:09:55 +0900

    Implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const [t] = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{t("hello")}</p>
+        <p>{t("world")}</p>
       </header>
     </div>
   );
